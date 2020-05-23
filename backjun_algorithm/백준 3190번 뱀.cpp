efbdf29c56f952653f4a39cd9c118336aa97c55f
kimhwangdae <khd206@ajou.ac.kr>
2020-05-23 11:02:35 +0900

    3190 upload

--- backjun_algorithm/백준 3190번 뱀.cpp
+++ backjun_algorithm/백준 3190번 뱀.cpp
@@ -1,118 +1,115 @@
 #include <iostream>
+#include <cstring>
 #include <queue>
 using namespace std;
+const int MAX = 101;
+int Map[MAX][MAX];
+int N, K,L;
+int Time = 0;
+int direction = 0;
+int L_direction = 0;
+queue<pair<int, char>>q;
+queue<pair<int, int>>snake;
+typedef struct Dir {
+	int y, x;
+}Dir;
+
+Dir moveDir[4] = { {0,1},{-1,0},{0,-1},{1,0} };
+
+int Move()
+{
+	int y = snake.back().first;
+	int x = snake.back().second;
+
+	int next_y = y+moveDir[direction].y;
+	int next_x = x+moveDir[direction].x;
+	
+	if (next_y >= 0 && next_y < N && next_x >= 0 && next_x < N&&Map[next_y][next_x]!=1)
+	{
+
+		if (Map[next_y][next_x] == 2)
+		{
+			Map[next_y][next_x] = 1;
+			snake.push({ next_y,next_x });
+		}
+		else if (Map[next_y][next_x] == 0)
+		{
+			Map[next_y][next_x] = 1;
+			Map[snake.front().first][snake.front().second] = 0;
+			snake.push({ next_y,next_x });
+			snake.pop();
+		}
+
+		return 0;
+	}
+	
+	return -1;
+}
+
+void checkDir()
+{
+	int change_time = q.front().first;
+	if (change_time != Time)
+	{
+		return;
+	}
+	char change_dir = q.front().second;
+	q.pop();
+	
+	if (change_dir == 'L')
+	{
+		direction++;
+		direction = direction % 4;
+	}
+	else if (change_dir == 'D')
+	{
+		direction = (3+direction)%4;
+	}
+
+	return;
+}
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int n; // 보드 크기
-	int k; // 사과 개수
-	int l; // 방향 변환 횟수
-
-	int map[101][101] = { 0, }; // 맵 기본값 : 0
-	pair<int, char> dir_change[101]; // 방향 변환
-	int cur_change_index = 0;
-
-	const int APPLE = 1;
-	const int SNAKE = -1;
-
-	int dx[4] = { 0, 1, 0, -1 };
-	int dy[4] = { 1, 0, -1, 0 };
-
-	//방향 전환 인덱스
-	//좌회전 할 경우, cur_direction = (cur_direction + 3) % 4;
-	//우회전 할 경우, cur_direction = (cur_direction + 1) % 4;
-	int cur_direction = 0;
-
-	//초기 뱀 위치
-	map[0][0] = SNAKE;
-	pair<int, int> head = { 0, 0 };
-	queue<pair<int, int>> snake;
-	snake.push(head);
-
-	cin >> n;
-	cin >> k;
-
-	for (int i = 0; i < k; i++)
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
 	{
-		int x, y; // 사과 위치
-
-		cin >> x >> y;
-		map[x - 1][y - 1] = APPLE;
+		for (int j = 0; j < N; j++)
+		{
+			Map[i][j] = 0;
+		}
 	}
-
-	cin >> l;
-	for (int i = 0; i < l; i++)
+	
+	Map[0][0] = 1;
+	snake.push({ 0,0 });
+	for (int i = 0; i < K; i++)
 	{
-		//방향변환
-		cin >> dir_change[i].first >> dir_change[i].second;
+		int temp_x, temp_y;
+		cin >> temp_y >> temp_x;
+		Map[temp_y-1][temp_x-1] = 2;
 	}
-
-	int sec = 0;
-
+	cin >> L;
+	for (int i = 0; i < L; i++)
+	{
+		int temp_time;
+		char temp_dir;
+		cin >> temp_time >> temp_dir;
+		q.push({ temp_time,temp_dir });
+	}
 	while (1)
 	{
-		sec++;
-
-		int nx = head.first + dx[cur_direction];
-		int ny = head.second + dy[cur_direction];
-
-		if (nx < 0 || ny < 0 || nx >= n || ny >= n)
+		Time++;
+		if (Move() == -1)
 		{
-			//벽이면 게임 끝
 			break;
 		}
-		else if (map[nx][ny] == SNAKE)
+
+		if (!q.empty())
 		{
-			//이동할 위치가 뱀의 몸의 일부분이면 게임 끝
-			break;
-		}
-		else if (map[nx][ny] != APPLE)
-		{
-			//사과가 없으면 꼬리가 이동
-			auto tail = snake.front();
-			snake.pop();
-
-			map[tail.first][tail.second] = 0;
-		}
-
-		//머리 한칸 이동
-		head = { nx, ny };
-		snake.push(head);
-
-		map[nx][ny] = SNAKE;
-
-
-		//진행 방향을 찾음
-		int dir_change_sec = dir_change[cur_change_index].first;
-		if (cur_change_index < l && dir_change_sec == sec)
-		{
-			char dir = dir_change[cur_change_index].second;
-			if (dir == 'L')
-			{
-				cur_direction = (cur_direction + 3) % 4;
-			}
-			else if (dir == 'D')
-			{
-				cur_direction = (cur_direction + 1) % 4;
-			}
-			cur_change_index++;
-		}
-
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << map[i][j] << " ";
-			}
-			cout << endl;
+			checkDir();
 		}
 	}
-
-	cout << endl;
-	cout << sec << '\n';
-
-	return 0;
+	cout << Time << endl;
 }
